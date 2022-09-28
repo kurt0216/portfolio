3303dde134b21e3619026bf3310fe2b1f46a0c18
Yuki <yukiwadana@outlook.com>
2022-09-28 20:39:24 +0800

    update blog/index.js to return empty blogs

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -18,7 +18,8 @@ import BlogPost from '../../components/blogPost'
 export async function getStaticProps() {
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
-  const allBlogsData = getSortedBlogsData()
+  // const allBlogsData = getSortedBlogsData()
+  const allBlogsData = []
 
   // By returning { props: posts }, the Blog component
   // will receive `posts` as a prop at build time
